543682083b80be528975d1871a42dc263269f279
avrhm <avrhmabeles@gmail.com>
2025-06-17 14:17:43 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// הצהרה על האובייקט "Android" כדי ש-TypeScript לא יתלונן שהוא לא קיים.
+// האובייקט הזה מוזרק על ידי אפליקציית האנדרואיד בזמן ריצה.
+declare const Android: any;
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +11,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-webview-appcd';
+
+    onTagVideosClick(): void {
+    console.log("Button clicked. Trying to call native Android code...");
+
+    // בדיקה האם האובייקט 'Android' קיים.
+    // זה יקרה רק כשהאתר רץ בתוך ה-WebView של האפליקציה שלנו.
+    if (typeof Android !== 'undefined' && Android.openCamera) {
+      console.log("Android interface found. Calling openCamera().");
+      // קריאה לפונקציית ה-Kotlin שחשפנו!
+      Android.openCamera();
+    } else {
+      // הודעת שגיאה למקרה שמריצים את האתר בדפדפן רגיל
+      console.error("Android interface is not available.");
+      alert("This feature is only available within the King Road App.");
+    }
+  }
 }
